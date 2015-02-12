e55eca7c160b4e49cf0c77ebb1c3abb59e8568d6
Patrick Kelso <psk@puppetlabs.com>
2015-02-12 13:45:29 +1100

    error checking

--- facts.d/git-version.sh
+++ facts.d/git-version.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 export GIT=$(which git)
+if [ $? -ne 0 ]; then
+  echo "git-version=absent"
+else
 export AWK=$(which awk)
 export GITV=$($GIT --version | $AWK '{print $3}')
 echo "git-version=$GITV"
+fi
